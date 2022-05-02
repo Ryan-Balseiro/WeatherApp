27a5245dda6da3ed4811602a8a3f28dd69b98475
consultant <ryan.balseiro@enhanceit.com>
2022-05-02 09:08:09 -0400

    - first attempt at network calling

--- app/src/main/java/com/example/weatherapp/model/remote/WeatherApi.kt
+++ app/src/main/java/com/example/weatherapp/model/remote/WeatherApi.kt
@@ -1,11 +1,21 @@
 package com.example.weatherapp.model.remote
 
+import com.example.weatherapp.model.WeatherResponse
+import com.example.weatherapp.model.remote.Network.API_KEY
 import com.example.weatherapp.model.remote.Network.END_POINT
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface WeatherApi {
 
     //@GET(END_POINT)
 
     //@GET(END_POINT)
+
+    @GET(END_POINT)
+    suspend fun getWeathers(
+        @Query("zip") zip_code: String,
+        @Query("units") units: String,
+        @Query("appid") api_key: String = API_KEY,
+    ) : WeatherResponse
 }
---
+++ app/src/main/java/com/example/weatherapp/toberemoved/SettingsFragment.kt
@@ -0,0 +1,29 @@
+package com.example.weatherapp.toberemoved
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.preference.PreferenceFragmentCompat
+import com.example.weatherapp.R
+
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+        val view = inflater.inflate(
+            R.xml.root_preferences,
+            container,
+            false
+        )
+        return view
+    }
+}
